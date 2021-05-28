d7c55494c768f5c7275c09b1e63c75d107a7e8e4
Prachi <prachi.koyande@neosoftmail.com>
2021-05-28 13:13:14 +0530

    Third Commit

---
+++ naming-server-1/src/main/java/com/prachi/microservices/namingserver1/NamingServer1Application.java
@@ -0,0 +1,15 @@
+package com.prachi.microservices.namingserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class NamingServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingServer1Application.class, args);
+	}
+
+}
---
+++ naming-server-1/src/test/java/com/prachi/microservices/namingserver1/NamingServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prachi.microservices.namingserver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
